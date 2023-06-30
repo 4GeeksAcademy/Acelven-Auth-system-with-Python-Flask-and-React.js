d0abb9813e578370dd581e150a405d4ce88e0e4d
Acelven <124097190+Acelven@users.noreply.github.com>
2023-06-30 18:40:22 +0000

    backend 100% functional,link to frontend in prog

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
+    names = db.Column(db.String(120), unique=False, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(220), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -15,7 +18,10 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "names": self.names,
+            "lastname": self.lastname,
             "email": self.email,
-
             # do not serialize the password, its a security breach
         }
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
